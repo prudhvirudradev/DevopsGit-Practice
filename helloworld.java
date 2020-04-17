b2ed1b91d4c6342b96716e56d1b49d661da8d3c7
Prudhvi <prudhvi.maddhuru123@gmail.com>
2020-04-17 06:20:40 +0000

    added comment line

--- helloworld.java
+++ helloworld.java
@@ -2,10 +2,12 @@
 class HelloWorld 
 { 
     // Your program begins with a call to main(). 
-    // Prints "Hello, World" to the terminal window. 
+    // Prints "Hello, World" to the terminal window.
+    // comment line. 
+ 
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
         System.out.println<"hello,world">; 
-} 
+}
 
